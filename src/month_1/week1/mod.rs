3101021606a5157e90ad27d2e787bf843f25bfe0
Lean 4 VS Code Extension <>
2025-05-30 17:52:43 +0530

    Initial commit

--- src/month_1/week1/mod.rs
+++ src/month_1/week1/mod.rs
@@ -4,3 +4,9 @@ pub mod ll;
 
 pub mod searching;
 pub mod sorting;
+
+// 3 - 5 easy medium problems daily (15 hours a week ) .
+// Maintain a pattern journal .
+// dive deep into solutions .
+// List potential edge cases.
+// self guided mock-sessions
---
+++ src/month_2/mod.rs
@@ -0,0 +1,3 @@
+pub mod adv_graph;
+pub mod backtrack;
+pub mod dp;
